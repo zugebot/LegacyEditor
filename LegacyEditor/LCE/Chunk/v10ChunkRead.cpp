d835acba163f5c4df9562fb8ce8f6555dabd980d
zugebot <zugebot2000@gmail.com>
2023-11-29 12:13:23 -0800

    messed with a few things
    rewrote v10ChunkRead.cpp to use a lambda to reduce code volume

--- LegacyEditor/LCE/Chunk/v10ChunkRead.cpp
+++ LegacyEditor/LCE/Chunk/v10ChunkRead.cpp
@@ -20,29 +20,17 @@ namespace universal {
         chunkData->chunkZ = chunkNBT->getPrimitive<i32>("zPos");
         chunkData->lastUpdate = chunkNBT->getPrimitive<i64>("LastUpdate");
 
-        auto* blockArray = chunkNBT->getByteArray("Blocks");
-        chunkData->oldBlocks = u8_vec(65536);
-        memcpy(&chunkData->oldBlocks[0], blockArray->array, 65536);
-
-        auto* blockDataArray = chunkNBT->getByteArray("Data");
-        chunkData->blockData = u8_vec(65536);
-        memcpy(&chunkData->blockData[0], blockDataArray->array, 256);
-
-        auto* heightMapArray = chunkNBT->getByteArray("HeightMap");
-        chunkData->heightMap = u8_vec(256);
-        memcpy(&chunkData->heightMap[0], heightMapArray->array, 256);
-
-        auto* biomeArray = chunkNBT->getByteArray("Biomes");
-        chunkData->biomes = u8_vec(256);
-        memcpy(&chunkData->biomes[0], biomeArray->array, 256);
-
-        auto* skyLightArray = chunkNBT->getByteArray("SkyLight");
-        chunkData->skyLight = u8_vec(32768);
-        memcpy(&chunkData->skyLight[0], skyLightArray->array, 32768);
-
-        auto* blockLightArray = chunkNBT->getByteArray("BlockLight");
-        chunkData->blockLight = u8_vec(32768);
-        memcpy(&chunkData->blockLight[0], blockLightArray->array, 32768);
+        auto createAndCopy = [](const auto* byteArray, size_t size) {
+            u8_vec result(size);
+            memcpy(&result[0], byteArray->array, size);
+            return result;
+        };
+        chunkData->oldBlocks = createAndCopy(chunkNBT->getByteArray("Blocks"), 65536);
+        chunkData->blockData = createAndCopy(chunkNBT->getByteArray("Data"), 256);
+        chunkData->heightMap = createAndCopy(chunkNBT->getByteArray("HeightMap"), 256);
+        chunkData->biomes = createAndCopy(chunkNBT->getByteArray("Biomes"), 256);
+        chunkData->skyLight = createAndCopy(chunkNBT->getByteArray("SkyLight"), 32768);
+        chunkData->blockLight = createAndCopy(chunkNBT->getByteArray("BlockLight"), 32768);
 
         chunkData->NBTData = new NBTBase(new NBTTagCompound(), NBTType::TAG_COMPOUND);
         auto* compound_root = chunkData->NBTData->toType<NBTTagCompound>();
